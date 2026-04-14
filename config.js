51512d6313eb4a3cf2ce369be371a9993ee46be9
João Vitor Dias <104777038+JoaoDias12@users.noreply.github.com>
2026-04-14 12:54:49 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBR0io-r_snZTWy1pe8A0dsb4awBpANDxs",
+  authDomain: "escala-pnae.firebaseapp.com",
+  databaseURL: "https://escala-pnae-default-rtdb.firebaseio.com",
+  projectId: "escala-pnae",
+  storageBucket: "escala-pnae.firebasestorage.app",
+  messagingSenderId: "289230851590",
+  appId: "1:289230851590:web:a638d6c6a8409d65803d87",
+};
